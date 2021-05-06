9f1186c7952dbf5481f009dc621894b27536405f
宇宙大王 <vicky@vicky-MacbookPro.local>
2021-05-06 16:43:46 +0800

    jvm memory and gc info

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author vicky
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        boolean is = true;
+        while(is) {
+            List<Object> list = new ArrayList<>();
+            list.add("hello world");
+        }
+    }
+}
